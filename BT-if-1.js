770fa4cd95ef7ff79d01a0a212129c450c8dec19
cuong0209992 <sandtimezone92@gmail.com>
2021-03-19 08:43:34 +0700

    Bài tập điều kiện 1

---
+++ BT-if-1.js
@@ -0,0 +1,98 @@
+/*// Bài 1
+
+let inputa = +prompt("nhập vào số a")
+let inputb = +prompt("nhập vào số b")
+
+if(inputa % inputb ==0){
+    alert("a chia hết cho b");
+}
+else{
+    alert("a không chia hết cho b");
+}*/
+
+/*//Bài 2
+let inputAge = +prompt("nhập tuổi")
+if(inputAge <16){
+    document.writeln("Chưa đủ tuổi vào lớp 10");
+}
+
+else {
+    document.writeln("Đủ tuổi vào lớp 10");
+}*/
+
+/*//Bài 3
+let inputNumber = +prompt("nhập số nguyên");
+if(inputNumber <0){
+   alert("số đó nhỏ hơn 0");
+}
+else {
+   alert("số đố lớn hơn 0");
+}
+*/
+/*//bài 4
+let inputFirst = +prompt('nhập số nguyên đầu tiên');
+let inputSencond = +prompt('nhập số nguyên thứ hai');
+let inputThird = +prompt('nhập số nguyên thứ ba');
+
+let max = inputFirst;
+
+if(max <inputSencond){
+   max = inputSencond;
+
+    if(max<inputThird){
+        max = inputThird;
+        alert("số thứ 3 lớn nhất")
+    }
+else {
+        alert("số thứ 2 lớn nhất")
+    }
+}
+else {
+   max = inputFirst;
+   alert("số thứ 1 lớn nhất")
+}*/
+/*//bài 5
+
+let testMark = +prompt("nhập điểm kiểm tra")
+let termMark = +prompt("nhập điểm giữa kỳ")
+let finalMark = +prompt("nhập điểm cuối kỳ")
+
+let avg = (testMark + termMark * 2 + finalMark * 3) / 6;
+
+if(7.5<=avg && avg<=10){
+    alert('điểm A');
+}
+else if(6.5<=avg && avg<7.5){
+    alert('điểm B');
+}
+else if(5.5<=avg && avg<6.4){
+    alert('điểm C');
+}
+else if(4<=avg && avg<5.4){
+    alert('điểm D')
+}
+else{
+    alert('điểm F');
+}*/
+
+/*//bài 6
+
+let doanhSo = +prompt('nhập doanh số bán được: '+ 'đơn vị triệu nhé baby');
+let hoaHong;
+if(doanhSo=>600){
+    hoaHong = doanhSo * 0.09;
+    alert('Hoa hồng bạn nhận được là '+hoaHong +' triệu');
+}
+
+else if(400<=doanhSo && doanhSo<600){
+    hoaHong = doanhSo * 0.05;
+    alert('Hoa hồng bạn nhận được là '+hoaHong +' triệu');
+}
+
+else if(100<=doanhSo && doanhSo<400){
+    hoaHong = doanhSo * 0.02;
+    alert('Hoa hồng bạn nhận được là '+hoaHong +' triệu');
+}
+else {
+    alert('bạn chưa đủ doanh số: ')
+}*/
